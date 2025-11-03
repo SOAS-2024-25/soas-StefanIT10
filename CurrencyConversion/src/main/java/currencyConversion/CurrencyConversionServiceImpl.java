aefd91007f105e35b67158085ae6d1b1e5324f46
StefanIT10 <stefan.s.cvetkovic@gmail.com>
2025-11-03 13:25:15 +0100

    v3

--- CurrencyConversion/src/main/java/currencyConversion/CurrencyConversionServiceImpl.java
+++ CurrencyConversion/src/main/java/currencyConversion/CurrencyConversionServiceImpl.java
@@ -6,11 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import api.dtos.CurrencyConversionDto;
 import api.dtos.CurrencyExchangeDto;
 import api.services.CurrencyConversionService;
+import util.exceptions.InvalidQuantityException;
 
 @RestController
 public class CurrencyConversionServiceImpl implements CurrencyConversionService {
@@ -20,11 +22,18 @@ public class CurrencyConversionServiceImpl implements CurrencyConversionService 
 	
 	@Override
 	public ResponseEntity<?> getConversion(String from, String to, BigDecimal quantity) {
+		
+		if(quantity.compareTo(BigDecimal.valueOf(300.0)) == 1) {
+			throw new InvalidQuantityException(String.format("Quantity of %s"
+					+ " is too large", quantity));
+		}
+		
 		String endPoint = "http://localhost:8000/currency-exchange?from=" + from +
 				"&to=" + to;
-		ResponseEntity<CurrencyExchangeDto> response = template.getForEntity(endPoint, CurrencyExchangeDto.class);
+		ResponseEntity<CurrencyExchangeDto> response;
+		response = template.getForEntity(endPoint, CurrencyExchangeDto.class);
 		
-		if(response.getStatusCode() == HttpStatus.NOT_FOUND) return new ResponseEntity("Unable to fetch exchange", HttpStatus.BAD_REQUEST);
+		
 		
 		return ResponseEntity.ok(new CurrencyConversionDto(response.getBody(), quantity));
 	
--- CurrencyExchange/src/main/java/currencyExchange/CurrencyExchangeServiceImpl.java
+++ CurrencyExchange/src/main/java/currencyExchange/CurrencyExchangeServiceImpl.java
@@ -1,5 +1,7 @@
 package currencyExchange;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import api.dtos.CurrencyExchangeDto;
 import api.services.CurrencyExchangeService;
+import util.exceptions.CurrencyDoesntExistException;
+import util.exceptions.NoDataFoundException;
 
 @RestController
 public class CurrencyExchangeServiceImpl implements CurrencyExchangeService{
@@ -18,12 +22,43 @@ public class CurrencyExchangeServiceImpl implements CurrencyExchangeService{
 	
 	@Override
 	public ResponseEntity<?> getCurrencyExchange(String from, String to) {
-		CurrencyExchangeModel dbResponse = repo.findByFromAndTo(from, to);
-		if(dbResponse == null) 
-			return new ResponseEntity<String>("Unable to find exchange rate FROM: " + from + " TO: " + to, HttpStatus.NOT_FOUND);
 		
+		String missingCurrency = null;
+		List<String> validCurrencies = repo.findAllDistinctCurrencies();		
+		// provera da li from parametar odgovara nekoj valuti
+		if(!isValidCurrency(from))
+			missingCurrency = from;
+		
+		// proveriti da li to parametar odgovara nekoj valuti
+		else if(!isValidCurrency(to))
+			missingCurrency = to;
+		
+		// provera da li je missing != null ako jeste bacanje domain exception-a
+		if(missingCurrency != null)
+			throw new CurrencyDoesntExistException(String.format("Currency %s"
+					+ " does not exist in the database", missingCurrency), validCurrencies);
+		
+		CurrencyExchangeModel dbResponse = repo.findByFromAndTo(from, to);
+		
+		if(dbResponse == null) 
+		{
+			throw new NoDataFoundException(String.format("Requested exchange rate"
+					+ " [%s to %s] does not exist", from,to), validCurrencies);
+			
+		}
+			
+			
 		CurrencyExchangeDto dto = new CurrencyExchangeDto(dbResponse.getFrom(), dbResponse.getTo(), dbResponse.getExchangeRate());
 		return ResponseEntity.ok(dto);
+	}
+	
+	public boolean isValidCurrency(String currency) {
+		List<String> currencies = repo.findAllDistinctCurrencies();
+		for(String s: currencies) {
+			if(s.equalsIgnoreCase(currency))
+				return true;
+		}
+		return false;
 	}
 	
 	
